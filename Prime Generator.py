609eaef6faa3399ffcac0ff860df6b113177ad4a
ansarkarzh <47853392+ansarkarzh@users.noreply.github.com>
2019-02-23 20:20:32 +0600

    Update Prime Generator.py

--- Prime Generator.py
+++ Prime Generator.py
@@ -1,18 +1,18 @@
 import math
-n=int(input())
-primes=[]
+n = int(input())
+primes = []
 for i in range(n):
-    a=list(map(int, input().split()))
-    for j in range(2, a[1]+1):
-        for k in primes:
-            if k>int(math.sqrt(j)+1):
+    a = list(map(int, input().split()))
+    for j in range(a[0], a[1]+1):
+        if j!=1:
+            for k in primes:
+                if k > int(math.sqrt(j)+1):
+                    primes.append(j)
+                    break
+                if (j % k == 0):
+                    break
+            else:
                 primes.append(j)
-                break
-            if (j%k==0):
-                break
-        else:
-            primes.append(j)
     for sym in primes:
-        if sym>=a[0]:
-            print(sym)
+        print(sym)
     primes.clear()
