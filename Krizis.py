8ab047ed63668b576c76e15b4f3ec0dd0bbc9944
ansarkarzh <47853392+ansarkarzh@users.noreply.github.com>
2019-04-02 15:32:29 +0600

    Update Krizis.py

--- Krizis.py
+++ Krizis.py
@@ -2,9 +2,11 @@ n, s = map(int, input().split())
 a = list(map(int, input().split()))
 counter = 0
 a.sort()
+summa = 0
 for i in range(len(a)):
-    if a[i] <= s:
-        s -= a[i]
+    if a[i] + summa <= s:
+        summa += a[i]
+        counter += 1
     else:
-        print(i)
         break
+print(counter)
