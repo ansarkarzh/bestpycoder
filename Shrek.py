bc12519b3f1ae425711d134816ac8ccfe757f3aa
ansarkarzh <47853392+ansarkarzh@users.noreply.github.com>
2019-04-09 16:27:00 +0600

    Update Shrek.py

--- Shrek.py
+++ Shrek.py
@@ -1,10 +1,7 @@
 n = int(input())
 b = list(map(int, input().split()))
 b.sort()
-first = 0
-second = 0
 half = n//2
-for i in range(half):
-    first += b[i]
-    second += b[i+half]
+first = sum(b[:half])
+second = sum(b[half:])
 print(second-first)
